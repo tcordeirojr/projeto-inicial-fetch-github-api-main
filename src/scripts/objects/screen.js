97560aa97dd371fe008d3d4b26744a7444a004f4
tcordeirojr <tedcordeirojr@gmail.com>
2025-04-25 02:12:35 -0300

    caminhos relativos

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -11,13 +11,16 @@ const screen = {
         let repositoriesItens = ""
         user.repositories.forEach(repo => 
             repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
-    
         if(user.repositories.length > 0){
             this.userProfile.innerHTML += `<div class= "repositories section">
             <h2>Repositorios</h2>
             <ul>${repositoriesItens}</ul>
         </div>`
         }
+},
+renderNotFound(){
+    this.userProfile.innerHTML = "<h3>Usuário não encontrado! </h3>"
+    
 }
 }
 
--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,5 +1,5 @@
 
-import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+import { baseUrl, repositoriesQuantity } from "../variables.js"
 async function getRepositories(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
     return await response.json()
